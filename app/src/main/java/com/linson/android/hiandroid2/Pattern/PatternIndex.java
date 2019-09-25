233acb0853b072f6f063db2cd004ac4cf1f4eda4
lsfv <lsfv@163.com>
2019-09-25 11:37:47 +0800

    commit

---
+++ app/src/main/java/com/linson/android/hiandroid2/Pattern/PatternIndex.java
@@ -0,0 +1,17 @@
+package com.linson.android.hiandroid2.Pattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.linson.android.hiandroid2.R;
+
+
+public class PatternIndex extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pattern_index);
+    }
+}
